3fdd14bedda2e98833b7d15d7888392cc69da5cc
Beccs Lee <beccslee@Beccss-MacBook-Pro.local>
2019-05-07 15:21:51 -0700

    Created C++ files

---
+++ cmake-build-debug/CMakeFiles/CS124_5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS124_5.dir/main.cpp.o"
+  "CS124_5.pdb"
+  "CS124_5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS124_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
